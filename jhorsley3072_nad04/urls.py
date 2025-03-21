a52c4d10a7aac34b310bd334950cdbef41c2d44f
Joshorsley <146381020+Joshorsley@users.noreply.github.com>
2025-03-21 13:07:48 -0400

    finished the second part of the setup

--- jhorsley3072_nad04/urls.py
+++ jhorsley3072_nad04/urls.py
@@ -15,8 +15,13 @@ Including another URLconf
     2. Add a URL to urlpatterns:  path('blog/', include('blog.urls'))
 """
 from django.contrib import admin
-from django.urls import path
+from django.urls import path, include 
+from django.conf import settings
+from django.conf.urls.satic import satic
 
 urlpatterns = [
     path('admin/', admin.site.urls),
 ]
+
+urlpatterns += satic(settings.STATIC_URL, document_root = settings.STATIC_ROOT)
+urlpatterns += satic(settings.MEDIA_URL, document_root = settings.MEDIA_ROOT)
