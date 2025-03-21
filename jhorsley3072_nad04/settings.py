c976dc10fe421b3d6d90055a100e6b973642d476
Joshorsley <146381020+Joshorsley@users.noreply.github.com>
2025-03-21 17:06:32 -0400

    finsihed the creating models portion of the tutorial

--- jhorsley3072_nad04/settings.py
+++ jhorsley3072_nad04/settings.py
@@ -122,7 +122,7 @@ USE_TZ = True
 
 STATIC_URL = 'static/'
 STATICFILES_DIRS = [
-    BASE_DIR / 'satic',
+    BASE_DIR / 'static',
     BASE_DIR / 'posts' / 'static',
     BASE_DIR / 'profiles' / 'static',
 ]
