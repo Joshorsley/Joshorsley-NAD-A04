70c623ceb6e281100a0c3443e0fb9e473956b349
Joshorsley <146381020+Joshorsley@users.noreply.github.com>
2025-03-23 18:48:35 -0400

    finished adding a form to the modal

--- jhorsley3072_nad04/settings.py
+++ jhorsley3072_nad04/settings.py
@@ -37,12 +37,17 @@ INSTALLED_APPS = [
     'django.contrib.sessions',
     'django.contrib.messages',
     'django.contrib.staticfiles',
+
     # our own apps
     'posts',
     'profiles',
     # 3rd party apps
     'crispy_forms',
+    'crispy_bootstrap5',
 ]
+
+CRISPY_ALLOWED_TEMPLATE_PACKS = 'bootstrap5'
+CRISPY_TEMPLATE_PACK = 'bootstrap5'
 
 MIDDLEWARE = [
     'django.middleware.security.SecurityMiddleware',
