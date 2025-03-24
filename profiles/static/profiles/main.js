41f4c97e7669a500c96be4749a8cc9277852f2d3
Joshorsley <146381020+Joshorsley@users.noreply.github.com>
2025-03-24 13:25:31 -0400

    finished updating the profile with Ajax

--- profiles/static/profiles/main.js
+++ profiles/static/profiles/main.js
@@ -1,1 +1,57 @@
 console.log('hello my profile')
+
+const avatarBox= document.getElementById('avatar-box')
+const alertBox = document.getElementById('alert-box')
+const profileForm = document.getElementById('profile-form')
+
+
+const bioInput = document.getElementById('id_bio')
+const avatarInput = document.getElementById('id_avatar')
+
+const getCookie = (name) => {
+    let cookieValue = null;
+    if (document.cookie && document.cookie !== '') {
+        const cookies = document.cookie.split(';');
+        for (let i = 0; i < cookies.length; i++) {
+            const cookie = cookies[i].trim();
+            if (cookie.substring(0, name.length + 1) === (name + '=')) {
+                cookieValue = decodeURIComponent(cookie.substring(name.length + 1));
+                break;
+            }
+        }
+    }
+    return cookieValue;
+}
+const csrftoken = getCookie('csrftoken');
+
+profileForm.addEventListener('submit', e=>{
+    e.preventDefault()
+
+    const formData = new FormData()
+    formData.append('csrfmiddlewaretoken', csrftoken)
+    formData.append('bio', bioInput.value)
+    formData.append('avatar', avatarInput.files[0])
+
+    $.ajax({
+        type: 'POST',
+        url: '',
+        enctype: 'multipart/form-data',
+        data: formData,
+        success: function(response){
+            console.log(response)
+            avatarBox.innerHTML = `
+                <img src="${response.avatar}" class="rounded" height="200px" width="auto" alt="${response.user.user}">
+            `
+            bioInput.value = response.bio
+            handleAlerts('success', 'your profile has been updated!')
+        },
+        error: function(error){
+            console.log(error)
+        },
+        processData: false,
+        contentType: false,
+        cache: false,
+    })
+
+
+})
