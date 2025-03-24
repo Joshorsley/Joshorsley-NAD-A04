3bdc9754bb868a63ddbf7146e4085a8701b94069
Joshorsley <146381020+Joshorsley@users.noreply.github.com>
2025-03-24 12:51:59 -0400

    finished my profile view registration + adding the template

--- profiles/views.py
+++ profiles/views.py
@@ -2,12 +2,13 @@ from django.shortcuts import render
 from .models import Profile
 from .forms import ProfileForm
 from django.http import JsonResponse
-# Create your views here.
 
 def my_profile_view(request):
     obj = Profile.objects.get(user=request.user)
     form = ProfileForm(request.POST or None, request.FILES or None, instance=obj)
-    if request.is_ajax():
+    
+    
+    if request.headers.get('x-requested-with') == 'XMLHttpRequest':
         if form.is_valid():
             instance = form.save()
             return JsonResponse({
@@ -15,6 +16,7 @@ def my_profile_view(request):
                 'avatar': instance.avatar.url,
                 'user': instance.user.username
             })
+    
     context = {
         'obj': obj,
         'form': form,
