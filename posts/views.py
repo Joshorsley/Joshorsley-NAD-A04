6bffcf313dea7a4f3c906b6f679f342f573f88ee
Joshorsley <146381020+Joshorsley@users.noreply.github.com>
2025-03-23 20:59:30 -0400

    completed working on the post detail page part 2

--- posts/views.py
+++ posts/views.py
@@ -61,6 +61,19 @@ def load_post_data_view(request, num_posts):
     # Add a default return for non-AJAX requests
     return JsonResponse({'error': 'Invalid request'}, status=400)
 
+def post_detail_data_view(request, pk):
+    obj = Post.objects.get(pk=pk)
+    data = {
+        'id': obj.id,
+        'title': obj.title,
+        'body': obj.body,
+        'author': obj.author.user.username,
+        'logged_in': request.user.username,
+
+    }
+    return JsonResponse({'data': data})
+
+
 def like_unlike_post(request):
     if request.headers.get('x-requested-with') == 'XMLHttpRequest':
         pk = request.POST.get('pk')
